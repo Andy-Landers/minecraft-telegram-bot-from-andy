cd494b29e87269ea03fd00a05c551fef4bd15b21
Andy Landers <51389339+Andy-Landers@users.noreply.github.com>
2023-07-21 17:15:49 +0800

    增加中文支持

---
+++ lang/zh.php
@@ -0,0 +1,9 @@
+<?php
+$lang = [
+  "no_players_connected" => "目前Minecraft服务器无人在线 :(",
+  "one_player_connected" => "1 名玩家在线",
+  "players_connected" => "在线",
+  "joined" => "登录Minecraft",
+  "disconnected" => "退出Minecraft",
+];
+?>
